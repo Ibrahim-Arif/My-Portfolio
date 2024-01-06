eda40a7e749c9cd045387238b2a1b6310d605527
Ibrahim Arif <ibrahimrana232@gmail.com>
2024-01-06 12:39:02 +0500

    @vercel/analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,36 @@
-import { useState } from 'react'
-import { ReactBricks } from 'react-bricks/frontend'
-import type { AppProps } from 'next/app'
-import config from '../react-bricks/config'
+import { useState } from "react"
+import { ReactBricks } from "react-bricks/frontend"
+import type { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
 
-import '../css/styles.css'
+import config from "../react-bricks/config"
+
+import "../css/styles.css"
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   // Color Mode Management
   const savedColorMode =
-    typeof window === 'undefined' ? '' : localStorage.getItem('color-mode')
-  const [colorMode, setColorMode] = useState(savedColorMode || 'light')
+    typeof window === "undefined" ? "" : localStorage.getItem("color-mode")
+  const [colorMode, setColorMode] = useState(savedColorMode || "light")
   const toggleColorMode = () => {
-    const newColorMode = colorMode === 'light' ? 'dark' : 'light'
+    const newColorMode = colorMode === "light" ? "dark" : "light"
     setColorMode(newColorMode)
-    localStorage.setItem('color-mode', newColorMode)
+    localStorage.setItem("color-mode", newColorMode)
   }
 
   const reactBricksConfig = {
     ...config,
-    isDarkColorMode: colorMode === 'dark',
+    isDarkColorMode: colorMode === "dark",
     toggleColorMode,
     contentClassName: `antialiased font-content ${colorMode} ${
-      colorMode === 'dark' ? 'bg-gray-900' : 'bg-white'
+      colorMode === "dark" ? "bg-gray-900" : "bg-white"
     }`,
   }
 
   return (
     <ReactBricks {...reactBricksConfig}>
       <Component {...pageProps} />
+      <Analytics />
     </ReactBricks>
   )
 }
