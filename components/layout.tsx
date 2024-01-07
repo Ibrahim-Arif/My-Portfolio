f7cd989baa47044bef1f84809b973473b4fa44d5
Ibrahim Arif <ibrahimrana232@gmail.com>
2024-01-07 14:32:15 +0500

    grammer mistake fixed

--- components/layout.tsx
+++ components/layout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  let temp = "test"
+  // let temp = "test"
 
   return (
     <div className="flex flex-col h-screen justify-between font-content antialiased">
